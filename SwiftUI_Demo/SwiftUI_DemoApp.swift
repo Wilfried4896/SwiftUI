54f66222373b11b575393c8f5242f10f42901fad
Wilfried4896 <odiwilfried@gmail.com>
2023-01-16 19:52:48 +0300

    Initial Commit

---
+++ SwiftUI_Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI_Demo
+//
+//  Created by Вилфриэд Оди on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
